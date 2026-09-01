1d7fa2cc79fdad3bee7eeba31587a6f0a3b27631
abunaim34 <farhanadnanfarabi@gmail.com>
2026-09-01 21:09:41 +0600

    Project setup

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { type NextFunction, type Application, type Request, type Response } from "express";
+import httpStatus from "http-status";
+import { notFound } from "./app/middleware/notFound";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+
+const app: Application = express();
+
+app.use(
+    cors({
+        // origin: config.frontend_url,
+        credentials: true,
+    }),
+);
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+
+        res.status(httpStatus.OK).json({
+            success: true,
+            message: "Zod Validation",
+            data: {}
+        });
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+})
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: "Welcome to NexusField System Backend",
+    });
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import app from "./app";
+import { prisma } from "./app/lib/prisma";
+import config from "./config";
+
+const PORT = config.port;
+
+const main = async () => {
+    try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully.");
+
+        // await redisClient.connect()
+        // console.log('Redis connected successfully.');
+
+        // await transporter.verify()
+        // console.log('Nodemailer connected successfully');
+
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+};
+
+main();
